257de9e01c8cfc99a540c91d0707c221f0a474d4
Joseph Sachs <jhalesachs@gmail.com>
2025-08-14 13:28:31 +0700

    [MIN-52] Reset logs each run, add shortcut for seeing kafka queue

--- docker/restart.sh
+++ docker/restart.sh
@@ -2,9 +2,15 @@
 
 cd /Users/josephsachs/documents/dev/minare/docker/ 
 docker compose down
+
 cd ..
 mvn package
+
 cd docker
+
+rm logs/websocket-server.log
+rm logs2/websocket-server.log
+
 docker compose build --no-cache
 docker compose up -d
 docker compose logs -f
