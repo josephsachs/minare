a3e65480dfa0c86fcce840a4e4e428cbaddefbfc
Joseph Sachs <jhalesachs@gmail.com>
2026-01-25 10:46:02 +0700

    [MIN-131] read-snapshots.sh takes database name parameter

--- docker/read-snapshots.sh
+++ docker/read-snapshots.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-docker exec -it mongodb mongosh --quiet --eval "var allSnapshots = []; db.getSiblingDB('node_graph').getCollectionNames().filter(c => c.startsWith('snapshot_')).forEach(c => { var meta = db.getSiblingDB('node_graph')[c].findOne({_id: 'metadata'}); if(meta) { allSnapshots.push({collection: c, createdAt: meta.createdAt.toNumber()}); } }); allSnapshots.sort((a,b) => a.createdAt - b.createdAt).forEach(s => { print('\\n=== ' + s.collection + ' (' + new Date(s.createdAt) + ') ==='); db.getSiblingDB('node_graph')[s.collection].find().forEach(printjson); });"
+if [ -z "$1" ]; then
+  echo "Error: Database name required"
+  echo "Usage: $0 <database_name>"
+  exit 1
+fi
+
+DB_NAME="$1"
+
+docker exec -it mongodb mongosh --quiet --eval "
+var dbName = '$DB_NAME';
+var allSnapshots = [];
+db.getSiblingDB(dbName).getCollectionNames().filter(c => c.startsWith('snapshot_')).forEach(c => {
+  var meta = db.getSiblingDB(dbName)[c].findOne({_id: 'metadata'});
+  if(meta) {
+    allSnapshots.push({collection: c, createdAt: meta.createdAt.toNumber()});
+  }
+});
+allSnapshots.sort((a,b) => a.createdAt - b.createdAt).forEach(s => {
+  print('\\n=== ' + s.collection + ' (' + new Date(s.createdAt) + ') ===');
+  db.getSiblingDB(dbName)[s.collection].find().forEach(printjson);
+});"
